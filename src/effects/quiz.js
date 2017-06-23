eb6846e77fdbe3c78eccc0d5aeb7f514fdd533fb
Rob Elkin <robelkin@gmail.com>
2017-06-23 15:30:22 +0100

    add info to the page

--- src/effects/quiz.js
+++ src/effects/quiz.js
@@ -1,3 +1,12 @@
-function startQuiz(state, raw, send, done) {
+function startQuiz() {
+
+    for(var key in localStorage) {
+      //document.getElementById("quiz-pic").innerHTML = localStorage[key]["Image"]
+      data = JSON.parse(localStorage[key])
+      console.log(data)
+
+      document.getElementById("source_word_quiz").textContent = data["en"]
+      document.getElementById("answer_word_quiz2").textContent = data["ol"]
+    }
     console.log("Let's do a quiz!")
 }
