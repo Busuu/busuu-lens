1384021d876db56a5b85577921c95886dac670c6
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-Bruce.local>
2017-06-23 16:56:01 +0100

    final product

--- src/effects/quiz.js
+++ src/effects/quiz.js
@@ -7,10 +7,21 @@ var num = 0
 return num
 }
 
+function reset_buttons(){
+	sleep(1000)
+document.getElementById("answer_word_quiz_button1").style.backgroundColor = "#2E8CCC";
+document.getElementById("answer_word_quiz_button1").style.transform = "scale(1)";
+document.getElementById("answer_word_quiz_button2").style.backgroundColor = "#2E8CCC";
+document.getElementById("answer_word_quiz_button2").style.transform = "scale(1)";
+document.getElementById("answer_word_quiz_button3").style.backgroundColor = "#2E8CCC";
+document.getElementById("answer_word_quiz_button3").style.transform = "scale(1)";	
+}
+
 function startQuiz() {
 	num_keys = check_local_storage()
-	bruces_bonus_sidebar
+
 	document.getElementById("bruces_bonus_sidebar").style.visibility = "hidden";
+	  document.getElementById("first-time").style.visibility = "hidden";
 
 	key1 = Math.round(Math.random() * (num_keys - 1) + 1);
 
@@ -37,18 +48,30 @@ function startQuiz() {
 
     console.log("Let's do a quiz!")
     document.getElementById("quiz_dialog").style.visibility = "visible";
+    reset_buttons()
 }
 
 function done_gone_wrong(id_yall){
+var ping = new Audio("sounds/fuckedit.mp3");
 document.getElementById(id_yall).style.backgroundColor = "red"
 document.getElementById(id_yall).style.transform = "scale(1.2)";
+ping.play()
 }
 
 function done_gone_right(id_yall){
+var ping = new Audio("sounds/Twinkle-sound-effect.mp3");
 document.getElementById(id_yall).style.backgroundColor = "green"
 document.getElementById(id_yall).style.transform = "scale(1.2)";
+ping.play()
 startQuiz()
 }
 
-nextQuestion()
-	
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
